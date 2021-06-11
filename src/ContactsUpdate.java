3186814e8fc6b683d6121a2811743fd3b16999b7
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-06-11 11:57:56 -0500

    handoff

--- src/ContactsUpdate.java
+++ src/ContactsUpdate.java
@@ -11,10 +11,8 @@ public class ContactsUpdate {
     }
 
     public static List<Contacts> contactsList = new ArrayList<>();
-
-
-
     public static void menu (Path path) {
+
         System.out.println("1. View contacts.");
         System.out.println("2. Add a new contact.");
         System.out.println("3. Search a contact by name.");
@@ -23,47 +21,36 @@ public class ContactsUpdate {
         System.out.println("Enter an option (1, 2, 3, 4 or 5):");
         Input input = new Input();
         int in = input.getInt(1, 5);
-        for(String selection : list.keySet()){
-            System.out.printf("You selected %n, %s", num, selection);
-        }
-        String selectionStart = "";
-        while(!list.containsKey(selectionStart)){
-            selectionStart = in.getString("What would you like to do?");
-            if(!list.containsKey(selectionStart)){
-                System.out.println("Selection invalid. \n Please try again.");
-            }
+        switch (in) {
             case 1:
                 //View Contacts List
-                viewContacts();
+                viewContacts(path);
                 break;
             case 2:
-                addContact();
+                System.out.println("Enter Name:");
+
+                addContact(path);
                 break;
             case 3:
-                searchContact();
+                searchContact(path);
                 break;
             case 4:
-                deleteContact();
+                deleteContact(path);
                 break;
             case 5:
-                exitProgram();
+                System.out.println("Bye Bye!");
+              System.exit(5);
+            }
+
         }
 
-        // TODO: 6/10/21 Build methods so that they return user selection form the main menu 
-        System.out.println(menu());
-//        switch (menu()){
-//
-//
-//        }
 
-        Scanner userInput = new Scanner(System.in);
-        int userChoice = userInput.nextInt();
-        return userChoice;
-    }
+
+
 
     public static void viewContacts(){
         System.out.println("Your selection was '1' : View Contacts");
-        contacts.readFile(co)
+//        contacts.readFile(co)
 
 
     }
@@ -86,6 +73,7 @@ public class ContactsUpdate {
 
 
     public static void main(String[] args) {
+
         Contacts contactInfo = new Contacts("Rose", "Barcus", "2105555555");
         System.out.println(contactInfo);
 
