7b2eabbe3105750de6c305ecd19bda3c71ffab00
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-06-11 16:07:34 -0500

    handoff

--- src/Contacts.java
+++ src/Contacts.java
@@ -16,7 +16,7 @@ public class Contacts  {
     }
 
     public String getPhoneNumber() {
-        return phoneNumber;
+        return phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1)-$2-$3");
     }
 
     public String getName() {
